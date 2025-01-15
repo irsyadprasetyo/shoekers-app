486cbd0ac046e90fa1e2ff2ec06df7937cf37255
irsyadprasetyo <icadskicad8@gmail.com>
2025-01-15 23:56:57 +0700

    fix paddinggst

--- app/src/main/java/com/example/uaspbobj/SplashActivity.java
+++ app/src/main/java/com/example/uaspbobj/SplashActivity.java
@@ -15,11 +15,10 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Delay for 3 seconds before moving to the main activity
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
-            finish(); // Close the SplashActivity
-        }, 3000); // 3000ms = 3 seconds
+            finish();
+        }, 1700);
     }
 }
